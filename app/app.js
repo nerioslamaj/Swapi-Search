df2ab34f4aebc194cb8a7f640aedea48ff50cac3
Nerios Lamaj <nerios.lamaj@gmail.com>
2018-03-12 18:52:34 +0000

    Initial Commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,37 @@
-'use strict';
+(function() {
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  var app = angular.module('starWars', ['ui.router']);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/cards');
+    $stateProvider
+
+      .state('cards', {
+        url: '/cards',
+        templateUrl: 'components/cards/cards.html'
+      })
+      .state('cards.card', {
+        url: '/item',
+        templateUrl: 'components/card/item.html'
+      })
+  });
+
+  app.controller('SwapiController', function($scope, $http){
+      $scope.$watchGroup(['search', 'element', 'page'], function() {
+        fetch();
+      });
+
+      $scope.search = 'a';
+      $scope.page = 1;
+      $scope.element = 'people';
+
+      function fetch(){
+        $http.get("https://swapi.co/api/" +  $scope.element + "/?search=" + $scope.search + "&page=" + $scope.page)
+        .then(function(response) {
+          $scope.details = response.data;
+          $scope.maxPage = $scope.details.count / 10;
+        });
+      }
+    });
+})();
